b1e52266c23ce57b25eaf465057b77dde8d9b914
GRJesus <rod.giu@hotmail.com>
2019-07-29 21:27:57 -0300

    Calculo Bhaskara pronto

--- js/index.js
+++ js/index.js
@@ -11,9 +11,18 @@ var b = parseInt(prompt("Digite o segundo valor"));
 var c = parseInt(prompt("Digite o terceiro valor"));
 var delta = b * b - 4 * a * c;
 
+var p1 = document.createElement ("p");
+var p2 = document.createElement ("p");
+
 if (delta<0) {
   document.body.append ("Não existem valores");
 } else if (delta>0) {
-  document.body.append ("x1 = " + ((-b + Math.sqrt(delta)) / (2*a)));
-  document.body.append ("x2 = " + ((-b - Math.sqrt(delta)) / (2*a)));
+  var x1 = ((-b + Math.sqrt(delta)) / (2*a));
+  p1.append ("Valor de X1 = " + x1);
+
+  var x2 = ((-b - Math.sqrt(delta)) / (2*a));
+  p2.append ("Valor de X2 = " + x2);
+
+  document.body.appendChild (p1);//aqui manda escrever dentro do html
+  document.body.appendChild (p2);//aqui manda escrever dentro do html
 }
